f30393f74894dc2330f904463d65740501ebe794
Andrew Zhurov <zhurov.andrew@gmail.com>
2025-12-18 10:40:33 +0300

    keeper: enable feature-javascript

--- examples/src/andrewzhurov/users/keeper.scm
+++ examples/src/andrewzhurov/users/keeper.scm
@@ -407,7 +407,7 @@
 
     ;; (feature-emacs-arei)
     (feature-clojure)
-    ;; (feature-javascript)
+    (feature-javascript)
     (feature-emacs-flymake)
     (feature-rust-foreign)
     ;; (feature-emacs-guix
