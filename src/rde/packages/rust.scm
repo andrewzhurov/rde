da6ec75353e6f984458578f88b9a16e21d9822db
Andrew Zhurov <zhurov.andrew@gmail.com>
2026-06-27 11:30:32 +0300

    fix: rust: terminate unterminated block comment
    The scratch block opened with #| at the wasm-pack experiments was closed
    with another #| instead of |#, leaving the module unreadable and breaking
    any config that loads (rde packages rust) (e.g. feature-rust-foreign).

--- src/rde/packages/rust.scm
+++ src/rde/packages/rust.scm
@@ -221,42 +221,44 @@
     (description "")
     (license license:epl1.0)))
 
-;; (use-modules (guix gexp))
-;; (use-modules (rde api store))
-;; (glibc-dynamic-linker)
-;; (parameterize ((%daemon-socket-uri "file:///var/guix/daemon-socket/socket"))
-;;   (build-with-store evcxr))
+#|
+(use-modules (guix gexp))
+(use-modules (rde api store))
+(glibc-dynamic-linker)
+(parameterize ((%daemon-socket-uri "file:///var/guix/daemon-socket/socket"))
+  (build-with-store evcxr))
 
 
-;; ((@ (rde api store) build-with-store) wasm-pack)
-;; (use-modules (guix scripts)
-;;              (guix ui)
-;;              (guix packages)
-;;              (guix derivations)
-;;              (guix store)
-;;              (guix scripts build)
-;;              (guix tests)
-;;              (web uri))
-;; (uri-reference?)
-;; (guix-build)
-;; (build wasm-pack)
-;; (with-status-verbosity (assoc-ref opts 'verbosity)
-;;   (with-store store
-;;     (build-derivations store
-;;                        (package->derivation wasm-pack))))
-;; (%daemon-socket-uri)
-;; (let ((store (open-connection "file:///var/guix/daemon-socket/socket")))
-;;   (lowered-gexp-inputs store (primitive-eval (lower-object wasm-pack))))
-;; (build-package wasm-pack
-;;                #:print-build-trace #t
-;;                #:print-extended-build-trace? #t
-;;                #:verbosity 100)
-;; (let ((store (open-connection "file:///var/guix/daemon-socket/socket")))
-;;   (build-derivations store
-;;                      wasm-pack ))
-;; (open-connection "file:///var/guix/daemon-socket/socket")
-;; (open-connection-for-tests)
-;; (with-store store (+ 1 1))
-;; (;;show-derivation-outputs
-;;  show-what-to-build*
-;;  (build-package wasm-pack))
+((@ (rde api store) build-with-store) wasm-pack)
+(use-modules (guix scripts)
+             (guix ui)
+             (guix packages)
+             (guix derivations)
+             (guix store)
+             (guix scripts build)
+             (guix tests)
+             (web uri))
+(uri-reference?)
+(guix-build)
+(build wasm-pack)
+(with-status-verbosity (assoc-ref opts 'verbosity)
+  (with-store store
+    (build-derivations store
+                       (package->derivation wasm-pack))))
+(%daemon-socket-uri)
+(let ((store (open-connection "file:///var/guix/daemon-socket/socket")))
+  (lowered-gexp-inputs store (primitive-eval (lower-object wasm-pack))))
+(build-package wasm-pack
+               #:print-build-trace #t
+               #:print-extended-build-trace? #t
+               #:verbosity 100)
+(let ((store (open-connection "file:///var/guix/daemon-socket/socket")))
+  (build-derivations store
+                     wasm-pack ))
+(open-connection "file:///var/guix/daemon-socket/socket")
+(open-connection-for-tests)
+(with-store store (+ 1 1))
+ (;;show-derivation-outputs
+ show-what-to-build*
+ (build-package wasm-pack))
+|#
